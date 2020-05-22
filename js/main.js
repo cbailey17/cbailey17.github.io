b2bc23e5d1bb41adfef0fa6b79ec070f5f172a72
cbailey17 <31027014+cbailey17@users.noreply.github.com>
2020-05-21 18:22:23 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,3 +2,12 @@ $(function(){
   $("#navigation-placeholder").load("/nav.html");
   $("#footer-placeholder").load("/footer.html");
 });
+
+$('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+
+});
+
